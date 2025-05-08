37fb23b5551c095e927458b7b805b04c02b2e5b7
tevben05 <stevemeir08@gmail.com>
2025-05-08 17:36:22 +0800

    Initial commit

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Steven Fischer'
+    return ('Steven Fischer'
+            '3rd year BSIT'
+            'System Integration and Architecture')
 
 if __name__ == '__main__':
     app.run(debug=True)
